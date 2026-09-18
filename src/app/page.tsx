7febcab3e7e842f5df94187e8e99263352fd1521
Himanshu-770 <himanshusrivastava211006@gmail.com>
2026-09-18 23:47:50 +0530

    change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import Landing from "./landing/page";
-import About from "./about/page";
 export default function Home() {
   return (
     <main className="min-h-screen bg-[var(--background)] text-[var(--text-primary)]">
